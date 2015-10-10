780aafa38976c9f546a975ccfbdb4c2da8d38e96
Vincent Min Tseng <mintseng@berkeley.edu>
2015-10-09 20:54:52 -0700

    I"M DONE

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+    
+    post "dogs/new", to: "dogs#create"
+    resources :dogs
+    
   get "users", to: "users#index"
   get "users/new", to: "users#new"
   get "users/:id", to: "users#show", as: :user
